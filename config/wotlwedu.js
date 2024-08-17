92578b79dc453dc8a5b4c3476887029bbdb8a010
Dave Kelly <wotlwedu@ravelox.co.uk>
2024-08-17 16:03:20 -0700

    Added default values to config file

--- config/wotlwedu.js
+++ config/wotlwedu.js
@@ -1,39 +1,37 @@
-const db_password = `${process.env.WOTLWEDU_DB_PASSWORD}`;
+module.exports.app_port = process.env.WOTLWEDU_APP_PORT || 9876 ;
+module.exports.app_listen = process.env.WOTLWEDU_APP_LISTEN || "0.0.0.0";
 
-module.exports.app_port = `${process.env.WOTLWEDU_APP_PORT}`;
-module.exports.app_listen = "0.0.0.0";
-
-module.exports.db_host = `${process.env.WOTLWEDU_DB_HOST}`;
-module.exports.db_user = "wotlwedu";
-module.exports.db_database = "wotlwedu";
-module.exports.db_password = db_password;
+module.exports.db_host = process.env.WOTLWEDU_DB_HOST || "localhost";
+module.exports.db_user = process.env.WOTLWEDU_DB_USER || "wotlwedu";
+module.exports.db_database = process.env.WOTLWEDU_DB_NAME || "wotlwedu";
+module.exports.db_password = process.env.WOTLWEDU_DB_PASSWORD
 
 module.exports.db_logging = false;
 
 module.exports.db_force_sync = true;
 
-module.exports.jwtSecret = `${process.env.WOTLWEDU_JWT_SECRET}`;
+module.exports.jwtSecret = process.env.WOTLWEDU_JWT_SECRET;
 module.exports.jwtExpiry = "1h";
 module.exports.jwtRefreshExpiry = "2h";
 
 module.exports.defaultItemsPerPage = 10;
 
-module.exports.mailerSESAccessKey = `${process.env.AWS_ACCESS_KEY_ID}`;
-module.exports.mailerSESSecretKey = `${process.env.AWS_SECRET_ACCESS_KEY}`;
+module.exports.mailerSESAccessKey = process.env.AWS_ACCESS_KEY_ID;
+module.exports.mailerSESSecretKey = process.env.AWS_SECRET_ACCESS_KEY;
 module.exports.mailerFromAddress = "admin@wotlwedu.net";
 module.exports.mailerDisplayName = "Wotlwedu Admin";
 
-baseApiUrl = "https://www.wotlwedu.com:9876/";
-baseFrontendUrl = "https://www.wotlwedu.com/";
+baseApiUrl = process.env.WOTLWEDU_API_URL || "https://www.wotlwedu.com:9876/";
+baseFrontendUrl = process.env.WOTLWEDU_FRONTEND_URL || "https://www.wotlwedu.com/";
 
 module.exports.baseFrontendUrl = baseFrontendUrl;
 module.exports.baseApiUrl = baseApiUrl;
-module.exports.imageURL = baseApiUrl + "images/";
-module.exports.imageDir = "public/images/";
+module.exports.imageURL = process.env.WOTLWEDU_IMAGE_URL || baseApiUrl + "images/";
+module.exports.imageDir = process.env.WOTLWEDU_IMAGE_DIR || "public/images/";
 
-module.exports.ssl = true;
-module.exports.sslKeyFile = "server.key";
-module.exports.sslCert = "server.cert";
+module.exports.ssl = process.env.WOTLWEDU_SSL || true;
+module.exports.sslKeyFile = process.env.WOTLWEDU_SSL_KEY || "server.key";
+module.exports.sslCert = process.env.WOTLWEDU_SSL_CERT || "server.cert";
 
 module.exports.defaultRoleName = "Default Role";
 
