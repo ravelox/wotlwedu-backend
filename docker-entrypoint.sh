5835b62b8079312a9ea5a58d3c12b87b79de54ab
Dave Kelly <wotlwedu@ravelox.co.uk>
2024-09-03 10:57:48 -0700

    Updated entrypoint script to initialise database with new scripts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -39,23 +39,36 @@ if [ $DATABASE_PRESENT -eq 1 ]
 then
    echo "Database exists"
 else
+
+    if [ -z "$WOTLWEDU_DB_ROOT_USER" ]
+    then
+        echo "WOTLWEDU_DB_ROOT_USER must be set"
+        exit 1
+    fi
+    if [ -z "$WOTLWEDU_DB_ROOT_PASSWORD" ]
+    then
+        echo "WOTLWEDU_DB_ROOT_PASSWORD must be set"
+    fi
+
     echo "Database not present"
     echo "Using parameters:"
     echo "      Database name: ${WOTLWEDU_DB_NAME}"
     echo "      Database user: ${WOTLWEDU_DB_USER}"
     echo "      Database password: ${WOTLWEDU_DB_PASSWORD}"
 
-    sed -e "s/##dbname##/${WOTLWEDU_DB_NAME}/" -e "s/##dbuser##/${WOTLWEDU_DB_USER}/" -e "s/##dbpassword##/${WOTLWEDU_DB_PASSWORD}/" sql/reset_database.sql.template > reset_database.sql
-    mariadb -h ${WOTLWEDU_DB_HOST} -u root < reset_database.sql
-
     export OLDDIR=$(pwd)
     cd model
-    node util-initdb
-    node util-setupuser
-    node util-addfriends
+    node util-resetdb
+    node util-createdb
     cd ${OLDDIR}
     
 fi
+
+export OLDDIR=$(pwd)
+cd model
+node util-updatedb
+cd ${OLDDIR}
+
 echo
 echo
 echo "Initialisation complete"
