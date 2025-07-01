4618915a554823ac16e9434765cf83fd700f44da
Dave Kelly <wotlwedu@ravelox.co.uk>
2025-06-30 19:08:16 -0700

    Initial API documentation

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.use(cors({ origin: true }));
 
 // Set static directory
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/docs', express.static(path.join(__dirname, 'docs')));
 
 // Stop the favicon request in its tracks
 app.get("/favicon.ico", (req, res) => res.status(204));
